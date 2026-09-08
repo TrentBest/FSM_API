d51f83de56d0dde61824b55377c1c55ae14649b2
The Singularity Workshop <the.tabest@gmail.com>
2026-09-08 09:09:19 -0700

    Add integer FSM handle lifecycle coverage

--- Tests/IntegerBacked/FSMHandle_Tests.cs
+++ Tests/IntegerBacked/FSMHandle_Tests.cs
@@ -4,47 +4,72 @@ using IntegerFSM = TheSingularityWorkshop.FSM_API.IntegerBacked.FSM;
 using IntegerFSMHandle = TheSingularityWorkshop.FSM_API.IntegerBacked.FSMHandle;
 using IntegerFSMState = TheSingularityWorkshop.FSM_API.IntegerBacked.FSMState;
 
-namespace FSM_API_Tests.IntegerBacked
+namespace TheSingularityWorkshop.FSM_API.Tests.IntegerBacked
 {
     [TestFixture]
     public class FSMHandle_Tests
     {
-        [Test]
-        public void Constructor_PreservesIntegerIdentity()
+        private sealed class TestContext : IStateContext
         {
-            var fsm = CreateBasicFSM();
-            var context = new TestContext();
-            var handle = new IntegerFSMHandle(fsm, context, 42);
+            public string Name { get; set; }
+            public int Context_ID => Name == null ? 0 : Name.GetHashCode();
+            public bool IsValid { get; set; } = true;
+        }
+
+        [Test]
+        public void Constructor_SetsInitialStateButDoesNotEnterIt()
+        {
+            var entered = false;
+            var fsm = new IntegerFSM(7);
+            fsm.AddState(new IntegerFSMState(10, _ => entered = true, null, null));
+            var handle = new IntegerFSMHandle(fsm, new TestContext(), 42);
 
             Assert.That(handle.Id, Is.EqualTo(42));
             Assert.That(handle.FSM_ID, Is.EqualTo(7));
             Assert.That(handle.CurrentStateID, Is.EqualTo(10));
-            Assert.That(handle.Context, Is.SameAs(context));
+            Assert.That(handle.HasEnteredCurrentState, Is.False);
+            Assert.That(entered, Is.False);
         }
 
         [Test]
-        public void Constructor_RejectsNullDefinition()
+        public void Initialize_EntersInitialStateAndMarksHandleEntered()
         {
-            Assert.That(
-                () => new IntegerFSMHandle(null, new TestContext()),
-                Throws.ArgumentNullException);
+            var entered = 0;
+            var fsm = new IntegerFSM(7);
+            fsm.AddState(new IntegerFSMState(10, _ => entered++, null, null));
+            var handle = new IntegerFSMHandle(fsm, new TestContext());
+
+            handle.Initialize();
+
+            Assert.That(handle.CurrentStateID, Is.EqualTo(10));
+            Assert.That(handle.HasEnteredCurrentState, Is.True);
+            Assert.That(entered, Is.EqualTo(1));
         }
 
         [Test]
-        public void Constructor_RejectsNullContext()
+        public void Initialize_IsIdempotent()
         {
-            Assert.That(
-                () => new IntegerFSMHandle(CreateBasicFSM(), null),
-                Throws.ArgumentNullException);
+            var entered = 0;
+            var fsm = new IntegerFSM(7);
+            fsm.AddState(new IntegerFSMState(10, _ => entered++, null, null));
+            var handle = new IntegerFSMHandle(fsm, new TestContext());
+
+            handle.Initialize();
+            handle.Initialize();
+
+            Assert.That(entered, Is.EqualTo(1));
         }
 
         [Test]
-        public void Update_FollowsIntegerTransition()
+        public void Initialize_DoesNotPreventLaterTransition()
         {
-            var fsm = CreateBasicFSM();
+            var fsm = new IntegerFSM(7);
+            fsm.AddState(new IntegerFSMState(10, null, null, null));
+            fsm.AddState(new IntegerFSMState(20, null, null, null));
             fsm.AddTransition(10, 20, _ => true);
             var handle = new IntegerFSMHandle(fsm, new TestContext());
 
+            handle.Initialize();
             handle.Update();
 
             Assert.That(handle.CurrentStateID, Is.EqualTo(20));
@@ -52,109 +77,18 @@ namespace FSM_API_Tests.IntegerBacked
         }
 
         [Test]
-        public void Update_WhenConditionFalse_RemainsInCurrentState()
-        {
-            var fsm = CreateBasicFSM();
-            fsm.AddTransition(10, 20, _ => false);
-            var handle = new IntegerFSMHandle(fsm, new TestContext());
-
-            handle.Update();
-
-            Assert.That(handle.CurrentStateID, Is.EqualTo(10));
-        }
-
-        [Test]
-        public void EvaluateConditions_FollowsIntegerTransitionWithoutUpdate()
-        {
-            var fsm = CreateBasicFSM();
-            var events = "";
-            fsm.GetState(10).SetOnUpdate(_ => events += "U");
-            fsm.GetState(10).SetOnExit(_ => events += "X");
-            fsm.GetState(20).SetOnEnter(_ => events += "E");
-            fsm.AddTransition(10, 20, _ => true);
-            var handle = new IntegerFSMHandle(fsm, new TestContext());
-
-            handle.EvaluateConditions();
-
-            Assert.That(handle.CurrentStateID, Is.EqualTo(20));
-            Assert.That(handle.HasEnteredCurrentState, Is.True);
-            Assert.That(events, Is.EqualTo("XE"));
-        }
-
-        [Test]
-        public void EvaluateConditions_WhenFalseRemainsInCurrentState()
-        {
-            var fsm = CreateBasicFSM();
-            var handle = new IntegerFSMHandle(fsm, new TestContext());
-            fsm.AddTransition(10, 20, _ => false);
-
-            handle.EvaluateConditions();
-
-            Assert.That(handle.CurrentStateID, Is.EqualTo(10));
-            Assert.That(handle.HasEnteredCurrentState, Is.True);
-        }
-
-        [Test]
-        public void TransitionTo_UsesIntegerIdentity()
-        {
-            var fsm = CreateBasicFSM();
-            var handle = new IntegerFSMHandle(fsm, new TestContext());
-
-            handle.TransitionTo(20);
-
-            Assert.That(handle.CurrentStateID, Is.EqualTo(20));
-            Assert.That(handle.HasEnteredCurrentState, Is.True);
-        }
-
-        [Test]
-        public void TransitionTo_RejectsMissingTargetState()
-        {
-            var handle = new IntegerFSMHandle(CreateBasicFSM(), new TestContext());
-
-            Assert.That(
-                () => handle.TransitionTo(999),
-                Throws.InvalidOperationException);
-            Assert.That(handle.CurrentStateID, Is.EqualTo(10));
-        }
-
-        [Test]
-        public void ResetFSMInstance_ReturnsToInitialIntegerState()
-        {
-            var fsm = CreateBasicFSM();
-            var handle = new IntegerFSMHandle(fsm, new TestContext());
-            handle.TransitionTo(20);
-
-            handle.ResetFSMInstance();
-
-            Assert.That(handle.CurrentStateID, Is.EqualTo(10));
-        }
-
-        [Test]
-        public void IsValid_ReflectsContextValidity()
+        public void IsValid_TracksCurrentContextValidity()
         {
             var context = new TestContext { IsValid = true };
-            var handle = new IntegerFSMHandle(CreateBasicFSM(), context);
+            var fsm = new IntegerFSM(7);
+            fsm.AddState(new IntegerFSMState(10, null, null, null));
+            var handle = new IntegerFSMHandle(fsm, context);
 
             Assert.That(handle.IsValid, Is.True);
 
             context.IsValid = false;
 
             Assert.That(handle.IsValid, Is.False);
-        }
-
-        private static IntegerFSM CreateBasicFSM()
-        {
-            var fsm = new IntegerFSM(7);
-            fsm.AddState(new IntegerFSMState(10, null, null, null));
-            fsm.AddState(new IntegerFSMState(20, null, null, null));
-            return fsm;
-        }
-
-        private sealed class TestContext : IStateContext
-        {
-            public string Name { get; set; } = "Test";
-            public int Context_ID => Name == null ? 0 : Name.GetHashCode();
-            public bool IsValid { get; set; } = true;
         }
     }
 }
