585dab4c535766bd559d9ed9184ef48bd46566b3
The Singularity Workshop <the.tabest@gmail.com>
2026-09-07 21:38:37 -0700

    Add atomic tests for integer-backed FSMTransition

---
+++ Tests/IntegerBacked/FSMTransition_Tests.cs
@@ -0,0 +1,71 @@
+using System;
+using NUnit.Framework;
+using TheSingularityWorkshop.FSM_API;
+
+namespace TheSingularityWorkshop.FSM_API.Tests.IntegerBacked
+{
+    [TestFixture]
+    public class FSMTransition_Tests
+    {
+        private sealed class TestContext : IStateContext
+        {
+            public string Name { get; set; }
+            public int Context_ID => Name == null ? 0 : Name.GetHashCode();
+            public bool IsValid { get; set; } = true;
+        }
+
+        [Test]
+        public void Constructor_PreservesIntegerEndpoints()
+        {
+            var transition = new FSMTransition(10, 20, _ => true);
+
+            Assert.That(transition.FromID, Is.EqualTo(10));
+            Assert.That(transition.ToID, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Constructor_AllowsZeroEndpoints()
+        {
+            var transition = new FSMTransition(0, 0, _ => true);
+
+            Assert.That(transition.FromID, Is.Zero);
+            Assert.That(transition.ToID, Is.Zero);
+        }
+
+        [Test]
+        public void Constructor_RejectsNullCondition()
+        {
+            Assert.Throws<ArgumentNullException>(() => new FSMTransition(1, 2, null));
+        }
+
+        [Test]
+        public void Evaluate_ReturnsConditionResult()
+        {
+            var transition = new FSMTransition(1, 2, context => context.IsValid);
+            var context = new TestContext { Name = "valid" };
+
+            Assert.That(transition.Evaluate(context), Is.True);
+
+            context.IsValid = false;
+            Assert.That(transition.Evaluate(context), Is.False);
+        }
+
+        [Test]
+        public void Condition_CanBeReplaced()
+        {
+            var transition = new FSMTransition(1, 2, _ => false);
+
+            transition.Condition = _ => true;
+
+            Assert.That(transition.Evaluate(new TestContext()), Is.True);
+        }
+
+        [Test]
+        public void ToString_UsesIntegerEndpoints()
+        {
+            var transition = new FSMTransition(7, 42, _ => true);
+
+            Assert.That(transition.ToString(), Is.EqualTo("7 --[Condition]--> 42"));
+        }
+    }
+}
